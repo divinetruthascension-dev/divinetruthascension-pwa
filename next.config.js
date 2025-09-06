b98c249339dedef5908f163c87a917695dc2ea47
divinetruthascension-dev <divinetruthascension@gmail.com>
2025-09-06 13:59:11 +1000

    modify to fix and deploy for build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
-/** @type {import('next').NextConfig} */
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Enables importing ECMAScript modules
+    config.module.rules.push({
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
 
-const isAndroid = process.platform === "android" || process.arch === "arm64";
-
-if (isAndroid) {
-  console.log("⚠️ Running on Termux (Android/ARM64) → Using Babel fallback.");
-} else {
-  console.log("✅ Using SWC compiler.");
-}
-
-const nextConfig = {
-  reactStrictMode: true,
+    return config;
+  },
 };
-
-module.exports = nextConfig;
